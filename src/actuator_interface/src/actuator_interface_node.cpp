fab9551db291f6a83ec5053e33bf9695327bde4a
phild <phild@protonmail.com>
2020-04-04 08:18:58 +0200

    configured open loop diff drive controller and tested on host

--- src/actuator_interface/src/actuator_interface_node.cpp
+++ src/actuator_interface/src/actuator_interface_node.cpp
@@ -13,14 +13,16 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh;
     nh.setCallbackQueue(&callbackQueue);
     std::string interfaceType;
-    nh.getParam("/robopi/actuator_interface/loop_hz", interfaceType);
+    nh.getParam("/robopi/actuator_interface/interface_type", interfaceType);
 
     if (interfaceType == "effort") {
+        ROS_INFO("Hardware interface set to effort.");
         ActuatorInterfaceEffort rhi(nh);
         ros::MultiThreadedSpinner spinner(0);
         spinner.spin(&callbackQueue);
         return 0;
     }else{
+        ROS_INFO("Hardware interface set to velocity.");
         ActuatorInterfaceVelocity rhi(nh);
         ros::MultiThreadedSpinner spinner(0);
         spinner.spin(&callbackQueue);
